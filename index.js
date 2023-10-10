f90e54a6b25bd961888fe5a1789aeee1ce015477
gaal-7 <paco_garcia_alonso@hotmail.com>
2023-10-10 12:50:34 -0600

    fisrt commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const cors = require('cors')
+const express = require('express')
+
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+
+const port = 8080;
+
+app.get('/', (req, res, next) => {
+	res.send('node express api <br> by adsoft');
+});
+
+
+app.get('/header', (req, res, next) => {
+  res.sendfile("assets/header.json");
+});
+
+app.get('/work-experience', (req, res, next) => {
+	res.sendfile("assets/work-experience.json");
+  });
+
+  app.get('/skills', (req, res, next) => {
+	res.sendfile("assets/skills.json");
+  });
+
+  app.get('/achievements', (req, res, next) => {
+	res.sendfile("assets/achievements.json");
+  });
+
+  app.get('/interest', (req, res, next) => {
+    res.sendfile("assets/interest.json");
+    });
+  
+
+
+app.listen(port,  () => 
+	console.log('listening on port ' + port
+));
