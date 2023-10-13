631170ea33e45008bd7b62f89af88ef7cf246daf
gaal-7 <paco_garcia_alonso@hotmail.com>
2023-10-13 10:19:25 -0600

    update

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const cors = require('cors')
-const express = require('express')
-
+const cors = require("cors");
+const express = require("express");
 
 const app = express();
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 const port = 8080;
 
-app.get('/', (req, res, next) => {
-	res.send('node express api <br> by adsoft');
+app.get("/", (req, res, next) => {
+  res.send("node express api <br> by adsoft");
 });
 
-
-app.get('/header', (req, res, next) => {
+app.get("/header", (req, res, next) => {
   res.sendfile("assets/header.json");
 });
 
-app.get('/work-experience', (req, res, next) => {
-	res.sendfile("assets/work-experience.json");
-  });
+app.get("/work-experience", (req, res, next) => {
+  res.sendfile("assets/work-experience.json");
+});
 
-  app.get('/skills', (req, res, next) => {
-	res.sendfile("assets/skills.json");
-  });
+app.get("/skills", (req, res, next) => {
+  res.sendfile("assets/skills.json");
+});
 
-  app.get('/achievements', (req, res, next) => {
-	res.sendfile("assets/achievements.json");
-  });
+app.get("/achievements", (req, res, next) => {
+  res.sendfile("assets/achievements.json");
+});
 
-  app.get('/interest', (req, res, next) => {
-    res.sendfile("assets/interest.json");
-    });
-  
+app.get("/interest", (req, res, next) => {
+  res.sendfile("assets/interest.json");
+});
 
+app.get("/hours", (req, res, next) => {
+  res.sendfile("assets/hours.json");
+});
 
-app.listen(port,  () => 
-	console.log('listening on port ' + port
-));
+app.get("/size", (req, res, next) => {
+  res.sendfile("assets/size.json");
+});
+
+app.listen(port, () => console.log("listening on port " + port));
